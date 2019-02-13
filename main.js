927d1addd392a835c4ddfe0ba47e522e2992a24d
Ester Fernandez <esterfernand95@gmail.com>
2019-02-13 14:05:46 +0100

    commit

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+const main = () => {
+  
+  console.log("heeey");
+  
+  const buildDom = (html) => {
+    const main = document.querySelector('main');
+    main.innerHTML = html; 
+
+    return main
+  };
+
+  const buildSplashScreen = () => {
+    const splashScreen = buildDom(`
+    <section class="splash-screen">
+    <h1>Eternal enemies</h1>
+    <button>Start</button>
+    </section>`);
+
+  };
+
+  buildSplashScreen();
+
+};
+
+
+window.addEventListener('load', main);
